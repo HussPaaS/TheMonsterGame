955bb213347c7777b1790ce59f1c845f2107ae71
HussPaaS <113926312+HussPaaS@users.noreply.github.com>
2021-12-11 22:29:43 +0800

    change time

--- example/position.py
+++ example/position.py
@@ -10,4 +10,7 @@ def main(stdscr):
     title = "Please choose your favorite programming language: "
     options = ["Java", "JavaScript", "Python", "PHP", "C++", "Erlang", "Haskell"]
     option, index = pick.pick(
- 
+        options,
+        title,
+        indicator="=>",
+        default_index=2,
