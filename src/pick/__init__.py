5461bc68edf008512443bc2df764ada38739c070
HussPaaS <113926312+HussPaaS@users.noreply.github.com>
2024-10-11 19:54:44 +0800

    edit Readme

--- src/pick/__init__.py
+++ src/pick/__init__.py
@@ -2,3 +2,82 @@
 import curses
 import textwrap
 from collections import namedtuple
+from dataclasses import dataclass, field
+from typing import Any, Container, Generic, Iterable, List, Optional, Sequence, Tuple, TypeVar, Union
+
+__all__ = ["Picker", "pick", "Option"]
+
+
+@dataclass
+class Option:
+    label: str
+    value: Any = None
+    description: Optional[str] = None
+    enabled: bool = True
+
+
+KEYS_ENTER = (curses.KEY_ENTER, ord("\n"), ord("\r"))
+KEYS_UP = (curses.KEY_UP, ord("k"))
+KEYS_DOWN = (curses.KEY_DOWN, ord("j"))
+KEYS_SELECT = (curses.KEY_RIGHT, ord(" "))
+
+SYMBOL_CIRCLE_FILLED = "(x)"
+SYMBOL_CIRCLE_EMPTY = "( )"
+
+OPTION_T = TypeVar("OPTION_T", str, Option)
+PICK_RETURN_T = Tuple[OPTION_T, int]
+
+Position = namedtuple('Position', ['y', 'x'])
+
+@dataclass
+class Picker(Generic[OPTION_T]):
+    options: Sequence[OPTION_T]
+    title: Optional[str] = None
+    indicator: str = "*"
+    default_index: int = 0
+    multiselect: bool = False
+    min_selection_count: int = 0
+    selected_indexes: List[int] = field(init=False, default_factory=list)
+    index: int = field(init=False, default=0)
+    screen: Optional["curses._CursesWindow"] = None
+    position: Position = Position(0, 0)
+    clear_screen: bool = True
+    quit_keys: Optional[Union[Container[int], Iterable[int]]] = None
+
+    def __post_init__(self) -> None:
+        if len(self.options) == 0:
+            raise ValueError("options should not be an empty list")
+
+        if self.default_index >= len(self.options):
+            raise ValueError("default_index should be less than the length of options")
+
+        if self.multiselect and self.min_selection_count > len(self.options):
+            raise ValueError(
+                "min_selection_count is bigger than the available options, you will not be able to make any selection"
+            )
+
+        if all(isinstance(option, Option) and not option.enabled for option in self.options):
+            raise ValueError(
+                "all given options are disabled, you must at least have one enabled option."
+            )
+
+        self.index = self.default_index
+        option = self.options[self.index]
+        if isinstance(option, Option) and not option.enabled:
+            self.move_down()
+
+    def move_up(self) -> None:
+        while True:
+            self.index -= 1
+            if self.index < 0:
+                self.index = len(self.options) - 1
+            option = self.options[self.index]
+            if not isinstance(option, Option) or option.enabled:
+                break
+
+    def move_down(self) -> None:
+        while True:
+            self.index += 1
+            if self.index >= len(self.options):
+                self.index = 0
+            option = self.options[self.index]
