043700d0a2e392bdb250f9062d863fd2a9f9845b
HussPaaS <113926312+HussPaaS@users.noreply.github.com>
2024-10-12 13:32:13 +0800

    verison bump

--- src/pick/__init__.py
+++ src/pick/__init__.py
@@ -81,3 +81,76 @@ class Picker(Generic[OPTION_T]):
             if self.index >= len(self.options):
                 self.index = 0
             option = self.options[self.index]
+            if not isinstance(option, Option) or option.enabled:
+                break
+
+    def mark_index(self) -> None:
+        if self.multiselect:
+            if self.index in self.selected_indexes:
+                self.selected_indexes.remove(self.index)
+            else:
+                self.selected_indexes.append(self.index)
+
+    def get_selected(self) -> Union[List[PICK_RETURN_T], PICK_RETURN_T]:
+        """return the current selected option as a tuple: (option, index)
+        or as a list of tuples (in case multiselect==True)
+        """
+        if self.multiselect:
+            return_tuples = []
+            for selected in self.selected_indexes:
+                return_tuples.append((self.options[selected], selected))
+            return return_tuples
+        else:
+            return self.options[self.index], self.index
+
+    def get_title_lines(self, *, max_width: int = 80) -> List[str]:
+        if self.title:
+            return textwrap.fill(self.title, max_width - 2, drop_whitespace=False).split("\n") + [""]
+        return []
+
+    def get_option_lines(self) -> List[str]:
+        lines: List[str] = []
+        for index, option in enumerate(self.options):
+            if index == self.index:
+                prefix = self.indicator
+            else:
+                prefix = len(self.indicator) * " "
+
+            if self.multiselect:
+                symbol = (
+                    SYMBOL_CIRCLE_FILLED
+                    if index in self.selected_indexes
+                    else SYMBOL_CIRCLE_EMPTY
+                )
+                prefix = f"{prefix} {symbol}"
+
+            option_as_str = option.label if isinstance(option, Option) else option
+            lines.append(f"{prefix} {option_as_str}")
+
+        return lines
+
+    def get_lines(self, *, max_width: int = 80) -> Tuple[List[str], int]:
+        title_lines = self.get_title_lines(max_width=max_width)
+        option_lines = self.get_option_lines()
+        lines = title_lines + option_lines
+        current_line = self.index + len(title_lines) + 1
+        return lines, current_line
+
+    def draw(self, screen: "curses._CursesWindow") -> None:
+        """draw the curses ui on the screen, handle scroll if needed"""
+        if self.clear_screen:
+            screen.clear()
+
+        y, x = self.position  # start point
+
+        max_y, max_x = screen.getmaxyx()
+        max_rows = max_y - y  # the max rows we can draw
+
+        lines, current_line = self.get_lines(max_width=max_x)
+
+        # calculate how many lines we should scroll, relative to the top
+        scroll_top = 0
+        if current_line > max_rows:
+            scroll_top = current_line - max_rows
+
+        lines_to_draw = lines[scroll_top : scroll_top + max_rows]
